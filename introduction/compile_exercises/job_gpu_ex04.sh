e742216506974017e283b7af434c941c08df1d4c
Johan Hellsvik <hellsvik@kth.se>
2026-08-20 10:24:03 +0200

    Uploaded 3_compile-run-Dardel

---
+++ introduction/compile_exercises/job_gpu_ex04.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -A edu26.summer      # Set the allocation to be charged for this job
+#SBATCH --reservation=edu26-08-20
+#SBATCH -J myjob             # Name of the job
+#SBATCH -p gpu               # The partition
+#SBATCH -t 00:10:00          # 1 hour wall-clock time
+#SBATCH --nodes=1            # Number of nodes
+#SBATCH --ntasks-per-node=1  # Number of MPI processes per node
+
+ml rocm/7.2.1                # Load a ROCm module
+ml craype-accel-amd-gfx90a   # set the accelerator target
+
+srun ./ex04.x > output.txt   # Run the ex04.x executable named myexe and write the output into output.txt
